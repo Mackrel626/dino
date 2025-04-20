c6bf1a6d7e557267589f4d5ae135568977333b82
Mackrel626 <enzik7763@gmail.com>
2025-04-20 15:57:45 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+W, H = 1280, 700
+FPS = 20
+
+window = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Dino Platformer")
+
+clock = pygame.time.Clock()
